055a67c0eb40fdbfe210ba2ab73cee998e3e3692
Tam Nguyen <tamntrung@gmail.com>
2023-05-08 22:33:54 +0000

    Completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,40 @@
+const width = 3;
+const height = 65;
+const area = width * height;
+console.log('Value of area:', area);
+console.log('Type of area:', typeof area);
+
+const bill = 14;
+const payment = 12;
+const change = bill - payment;
+console.log('Value of change:', change);
+console.log('Type of change:', typeof change);
+
+const quizzes = 92;
+const midterm = 98;
+const final = 76;
+const grade = (quizzes + midterm + final) / 3;
+
+console.log('Value of grade:', grade);
+console.log('Type of grade:', grade);
+
+const firstName = 'Tam';
+const lastName = 'Nguyen';
+const fullName = `${firstName} ${lastName}`;
+console.log('Value of fullName:', fullName);
+console.log('Type of fullName:', typeof fullName);
+
+const pH = 7.4;
+const isAcidic = pH < 7;
+console.log('Value of isAcidic:', isAcidic);
+console.log('Type of is Acidic:', typeof isAcidic);
+
+const headCount = 100;
+const isSparta = headCount === 300;
+console.log('Value of isSparta:', isSparta);
+console.log('Type of isSparta:', typeof isSparta);
+
+let motto = fullName;
+motto = motto + ' is the GOAT.';
+console.log('Value of motto:', motto);
+console.log('Type of motto:', typeof motto);
