0655a77d6d33a8e18d1043772b6534f9718f665f
Tam Nguyen <tamntrung@gmail.com>
2023-05-09 21:08:43 +0000

    Completed javascript-data-model

---
+++ javascript-data-model/bookshelf.js
@@ -0,0 +1,26 @@
+var bookshelf = [
+  {
+    isbn: '978-1491929483',
+    title:
+      'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+    author: 'Lindsay Bassett',
+  },
+  {
+    isbn: '978-1484218624',
+    title: 'JSON Quick Syntax Reference',
+    author: 'Wallace Jackson',
+  },
+  {
+    isbn: '978-0692232699',
+    title:
+      "Build APIs You Won't Hate: Everyone and their dog wants an API, so you should probably learn how to build them",
+    author: 'Phil Sturgeon & Laura Bohill',
+  },
+];
+
+console.log('Author os JSON Quick Syntax Referece:', bookshelf[1].author);
+console.log(
+  'ISBN of Introduction to Javascript Object Notation: A To-the-Point Guide to JSON:',
+  bookshelf[0].isbn
+);
+console.log('Title of Third Book:', bookshelf[2].title);
---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,63 @@
+const orderHistory = [
+  {
+    orderNumber: '114-3941689-8772232',
+    orderPlaced: 'August 4, 2020',
+    orderTotal: '$34.00',
+    shipTo: 'JS Masher',
+    items: [
+      {
+        name: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr.Axel',
+        returnwindow: 'Return window closed on Aug 19, 2020',
+        cost: '$31.55',
+      },
+    ],
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    orderPlaced: 'July 20, 2020',
+    orderTotal: '$44.53',
+    shipTo: 'JS Masher',
+    items: [
+      {
+        name: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnwindow: 'Return window closed on Aug 19, 2020',
+        cost: '$41.33',
+      },
+    ],
+  },
+  {
+    orderNumber: '114-2875557-9059409',
+    orderPlaced: 'July 4, 2020',
+    orderTotal: '$17.22',
+    shipTo: 'JS Masher',
+    items: [
+      {
+        name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        returnwindow: 'Return window closed on Aug 5, 2020',
+        cost: '$15.98',
+      },
+    ],
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    orderPlaced: 'July 3, 2020',
+    orderTotal: '$138.93',
+    shipTo: 'JS Masher',
+    items: [
+      {
+        name: 'GameCube Controller-Supermash Bros. Edition (Nintendo Switch)',
+        returnwindow: 'Return window closed on Aug 4, 2020',
+        cost: '$94.95',
+      },
+      {
+        name: 'The Art of Sql',
+        returnwindow: 'Return window closed on Aug 4, 2020',
+        author: 'Faroult, Stephanie',
+      },
+    ],
+  },
+];
+
+console.log(orderHistory);
