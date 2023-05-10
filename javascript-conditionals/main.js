f4b7b927ae3eed12d7d15ced9fdcf737a186688a
Tam Nguyen <tamntrung@gmail.com>
2023-05-10 22:06:02 +0000

    Corrected pH conditionals

--- javascript-conditionals/main.js
+++ javascript-conditionals/main.js
@@ -36,9 +36,9 @@ function isOldEnoughToDrinkAndDrive(person) {
 function categorizeAcidity(pH) {
   if (pH === 7) {
     return 'neutral';
-  } else if (pH < 7) {
+  } else if (pH < 7 && pH >= 0) {
     return 'acid';
-  } else if (pH > 7) {
+  } else if (pH > 7 && pH <= 14) {
     return 'base';
   } else {
     return 'invalid pH level';
