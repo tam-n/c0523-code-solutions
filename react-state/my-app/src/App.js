c7f82330af4e2541fe1934bdffa85c8a0356f6f7
Tam Nguyen <tamntrung@gmail.com>
2023-06-28 08:48:27 +0000

    Completed react-state exercise

--- react-state/my-app/src/App.js
+++ react-state/my-app/src/App.js
@@ -4,7 +4,9 @@ import ToggleButton from './ToggleButton';
 function App() {
   return (
     <div className="App">
-      <ToggleButton text="test" />
+      <ToggleButton color="red" text="btn 1" />
+      <ToggleButton color="blue" text="btn 2" />
+      <ToggleButton color="green" text="btn 3" />
     </div>
   );
 }
