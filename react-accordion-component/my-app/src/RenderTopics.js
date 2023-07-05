6ef009bc77198ee07be5f501f73c5bc7907a64fa
Tam Nguyen <tamntrung@gmail.com>
2023-07-05 06:42:37 +0000

    Completed react-accordion-component

---
+++ react-accordion-component/my-app/src/RenderTopics.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export default function RenderTopics(props) {
+  const [selected, setSelected] = useState(null);
+  console.log(selected);
+
+  const listTopics = props.topics.map((element) => {
+    return (
+      <div className="topics-wrapper" key={element.title}>
+        <div
+          className="title"
+          onClick={() => {
+            if (selected === element) {
+              return setSelected(null);
+            }
+            setSelected(element);
+          }}>
+          {element.title}
+        </div>
+        <div className={selected === element ? 'content' : 'hide'}>
+          {element.content}
+        </div>
+      </div>
+    );
+  });
+
+  return <>{listTopics}</>;
+}
