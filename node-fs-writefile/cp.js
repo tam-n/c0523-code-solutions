e670dd1ce6789c2061b4926747825989de37ad4b
Tam Nguyen <tamntrung@gmail.com>
2023-07-07 16:24:14 +0000

    Removed import.meta.url and changed readFile

--- node-fs-writefile/cp.js
+++ node-fs-writefile/cp.js
@@ -2,8 +2,7 @@ import { writeFile, readFile } from 'node:fs/promises';
 
 try {
   const [, , file, fileCopy] = process.argv;
-  const filePath = new URL(file, import.meta.url);
-  const content = await readFile(filePath, { encoding: 'utf8' });
+  const content = await readFile(file, 'utf8');
 
   await writeFile(fileCopy, content);
 } catch (error) {
