cf9b554673c00718330cd0c401c780f3bcfdb3cb
Tam Nguyen <tamntrung@gmail.com>
2023-07-11 16:47:24 +0000

    Added error handling

--- express-post-json/server.js
+++ express-post-json/server.js
@@ -4,18 +4,21 @@ let nextId = 1;
 const grades = {};
 
 const app = express();
+app.use(express.json());
 
 app.get('/api/grades', (req, res) => {
   res.json(grades);
 });
 
-app.use(express.json());
-
 app.post('/api/grades', (req, res) => {
   const newGrade = req.body;
-  newGrade.id = nextId++;
-  grades[newGrade.id] = newGrade;
-  res.status(200).json(grades);
+  if (!newGrade.name || !newGrade.course || !newGrade.score) {
+    res.status(400).send('Incomplete request');
+  } else {
+    newGrade.id = nextId++;
+    grades[newGrade.id] = newGrade;
+    res.status(200).json(newGrade);
+  }
 });
 
 app.listen(8080, () => {
