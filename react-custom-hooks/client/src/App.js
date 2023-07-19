041a387b9bd3be771d9336c7f1cb54928363487f
Tam Nguyen <tamntrung@gmail.com>
2023-07-19 08:52:35 +0000

    Completed react-custom-hooks exercise

---
+++ react-custom-hooks/client/src/App.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars -- Remove me */
+import { useState } from 'react';
+import PageTitle from './components/PageTitle';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import { useTodos } from './components/useTodos';
+
+export default function App() {
+  // TODO: Use the custom hook here instead of state and functions
+  const manageTodos = useTodos();
+  const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+  async function addTodo() {}
+  async function toggleCompleted() {}
+
+  if (manageTodos.error) {
+    console.error('Fetch error:', error);
+    return <div>Error! {error.message}</div>;
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col pt-5">
+          <PageTitle text="Todo App" />
+          <TodoForm onSubmit={manageTodos.addTodo} />
+          {manageTodos.isLoading && <div>Loading...</div>}
+          {!manageTodos.isLoading && (
+            <TodoList
+              todos={manageTodos.todos}
+              toggleCompleted={manageTodos.toggleCompleted}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
